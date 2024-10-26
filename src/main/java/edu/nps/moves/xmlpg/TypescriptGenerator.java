04d723cda703a4243030963c4caa4e47c609fc4f
couriourc <58319769+couriourc@users.noreply.github.com>
2024-10-26 07:46:03 +0000

    feature: initial ts generator

---
+++ src/main/java/edu/nps/moves/xmlpg/TypescriptGenerator.java
@@ -0,0 +1,20 @@
+package edu.nps.moves.xmlpg;
+
+import java.util.*;
+
+/**
+ *
+ * @author DMcG
+ */
+public class TypescriptGenerator extends Generator {
+    
+    
+    public TypescriptGenerator(HashMap tsClassDescriptions, Properties typescriptProperties) {
+        // initial typescript info generator 
+    }
+
+    @Override
+    public void writeClasses() {
+
+    }
+}
